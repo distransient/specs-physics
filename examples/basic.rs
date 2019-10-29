b04e102433422abab5c15a6b8731b79ee1a8dba8
Kel <kel@unclear.info>
2019-10-29 12:22:46 -0400

    Switch basic example to use code from ball3 nphysics example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,15 +1,18 @@
 extern crate log;
 extern crate simple_logger;
 
-use specs::{Builder, DispatcherBuilder, Join, ReadStorage, System, World, WorldExt, WriteStorage};
+use specs::{
+    Builder, DispatcherBuilder, Join, ReadExpect, ReadStorage, System, World, WorldExt,
+    WriteStorage,
+};
 use specs_physics::{
-    ncollide::shape::{Ball, ShapeHandle},
+    ncollide::shape::{Ball, Cuboid, ShapeHandle},
     nphysics::{
-        algebra::ForceType,
-        math::{Force, Vector},
-        object::{ColliderDesc, RigidBodyDesc},
+        math::Vector,
+        object::{ColliderDesc, Ground, RigidBody, RigidBodyDesc},
     },
     systems::PhysicsBundle,
+    world::MechanicalWorldRes,
     BodyComponent, EntityBuilderExt, SimplePosition,
 };
 
@@ -18,13 +21,16 @@ fn main() {
     // This will contain our Resources and Entities
     let mut world = World::new();
 
+    /*
+     * Specs Dispatcher.
+     */
     // Create the dispatcher with our systems on it
     let mut dispatcher_builder =
         DispatcherBuilder::new().with(MyPhysicsSystem, "my_physics_system", &[]);
 
     // Attach the specs-physics systems to the dispatcher,
     // with our pre-physics-stepping systems as a dependency
-    PhysicsBundle::<f32, SimplePosition<f32>>::default()
+    PhysicsBundle::<f32, SimplePosition<f32>>::new(Vector::y() * -9.81)
         .with_deps(&["my_physics_system"])
         .register(&mut world, &mut dispatcher_builder);
 
@@ -36,25 +42,73 @@ fn main() {
             &["physics_pose_system"],
         )
         .build();
+
+    // Sets up all resources for our dispatcher.
     dispatcher.setup(&mut world);
 
-    // Build our physics data
-    let body = RigidBodyDesc::new().translation(Vector::x() * 2.0).build();
-    let shape = ShapeHandle::new(Ball::new(1.6));
-    let collider_desc = ColliderDesc::new(shape);
+    /*
+     * Physics Ground.
+     */
+    let ground_thickness = 0.2;
 
-    // Create an entity and attach that data to it
+    // Create collider description for ground
+    let ground_shape = ShapeHandle::new(Cuboid::new(Vector::new(3.0, ground_thickness, 3.0)));
+    let ground_collider =
+        ColliderDesc::new(ground_shape).translation(Vector::y() * -ground_thickness);
+
+    // Create ground body in our world and attach our ground collider
     world
         .create_entity()
         .with(SimplePosition::<f32>::default())
-        .with_body::<f32, _>(body)
-        .with_collider::<f32>(&collider_desc)
+        .with_body::<f32, _>(Ground::new())
+        .with_collider::<f32>(&ground_collider)
         .build();
 
-    // Execute the dispatcher like this in your application loop
-    for _ in 0..200 {
-        dispatcher.dispatch(&world);
+    /*
+     * Physics Balls.
+     */
+    // Some values used to build the balls
+    let ball_amount: usize = 8;
+    let ball_radius = 0.1;
+    let ball_shift = (ball_radius + ColliderDesc::<f32>::default_margin()) * 2.0 + 0.002;
+    let ball_center = (
+        ball_shift * (ball_amount as f32) / 2.0,
+        ball_shift / 2.0,
+        ball_shift * (ball_amount as f32) / 2.0,
+    );
+    let ball_height = 3.0;
+
+    // Create collider description we can reuse per ball
+    let ball_collider = ColliderDesc::new(ShapeHandle::new(Ball::new(ball_radius))).density(1.0);
+
+    // Create ball rigidbodies and add them to our world
+    for i in 0..ball_amount {
+        for j in 0..ball_amount {
+            for k in 0..ball_amount {
+                world
+                    .create_entity()
+                    .with(SimplePosition::<f32>::default())
+                    .with_body(
+                        RigidBodyDesc::new()
+                            // Offset each body to build a cube of balls
+                            .translation(Vector::new(
+                                i as f32 * ball_shift - ball_center.0,
+                                j as f32 * ball_shift + ball_center.1 + ball_height,
+                                k as f32 * ball_shift - ball_center.2,
+                            ))
+                            .build(),
+                    )
+                    .with_collider(&ball_collider)
+                    .build();
+            }
+        }
     }
+
+    /*
+     * Running
+     */
+    // Execute the dispatcher in your application loop
+    dispatcher.dispatch(&world);
 }
 
 struct MyPhysicsSystem;
@@ -63,10 +117,10 @@ impl<'s> System<'s> for MyPhysicsSystem {
     type SystemData = WriteStorage<'s, BodyComponent<f32>>;
 
     fn run(&mut self, mut bodies: Self::SystemData) {
-        for body in (&mut bodies,).join() {
-            // Operate on our bodies.
-            body.0
-                .apply_force(0, &Force::linear(Vector::x()), ForceType::Force, true)
+        for (body,) in (&mut bodies,).join() {
+            if let Some(_rigid_body) = body.downcast_mut::<RigidBody<f32>>() {
+                // Operate on our bodies.
+            }
         }
     }
 }
@@ -74,11 +128,20 @@ impl<'s> System<'s> for MyPhysicsSystem {
 struct MyRenderingSystem;
 
 impl<'s> System<'s> for MyRenderingSystem {
-    type SystemData = ReadStorage<'s, SimplePosition<f32>>;
+    type SystemData = (
+        ReadStorage<'s, SimplePosition<f32>>,
+        ReadExpect<'s, MechanicalWorldRes<f32>>,
+    );
 
-    fn run(&mut self, positions: Self::SystemData) {
+    fn run(&mut self, (positions, mechanical_world): Self::SystemData) {
         for pos in (&positions,).join() {
-            println!("(Technically) Rendering {:?}", pos);
+            println!(
+                "Body Position (X: {:?}, Y: {:?}, Z: {:?}) @ {:?}s",
+                pos.0.translation.vector.x,
+                pos.0.translation.vector.y,
+                pos.0.translation.vector.z,
+                mechanical_world.integration_parameters.t,
+            );
         }
     }
 }
